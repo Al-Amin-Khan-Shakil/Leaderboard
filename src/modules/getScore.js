cd6b4308f6628d1ee3cbc34bbac79f55b5bd75bd
Al Amin Khan <alaminkhanshakil@gmail.com>
2023-09-03 02:55:20 +0600

    Change the game id of the api

--- src/modules/getScore.js
+++ src/modules/getScore.js
@@ -1,5 +1,5 @@
 const getScore = async () => {
-  const scoreURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/9Zauo9h1pY6f15rihTkl/scores/';
+  const scoreURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/L53H15QxkWnGNnJ93Di0/scores/';
 
   const response = await fetch(scoreURL);
   const data = await response.json();
